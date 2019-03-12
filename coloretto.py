1533a44b7ee91b3d83c1806a081021672e0f17d2
VisualBasist <VisualBasist@users.noreply.github.com>
2019-03-12 18:04:12 +0900

    列カードとちょっとマジックナンバー消し

--- coloretto.py
+++ coloretto.py
@@ -2,12 +2,15 @@ import random
 import itertools
 class Coloretto:
     class Player:
-        pass
+        def __init__(self):
+            self.hand=[]
     def __init__(self):
-        self.stock=list(itertools.chain.from_iterable([i]*9 for i in range(5)))
+        self.stock=list(itertools.chain.from_iterable([i]*9 for i in range(7-2)))
         random.shuffle(self.stock)
         print(self.stock)
         self.players=[Coloretto.Player() for i in range(2)]
         print(self.players)
+        self.rowcards=[1,2,3]
+        print(self.rowcards)
 
 game=Coloretto()
