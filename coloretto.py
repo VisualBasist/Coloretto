947a89d2f31559b7f8155dcc2edf2455a12e4cda
VisualBasist <VisualBasist@users.noreply.github.com>
2019-03-13 22:49:52 +0900

    行動とisbot

--- coloretto.py
+++ coloretto.py
@@ -2,17 +2,29 @@ import random
 import itertools
 class Coloretto:
     class Player:
-        def __init__(self):
+        def __init__(self,isbot=False):
             self.hand=[]
+            self.isbot=isbot
         def take_firstcard(self,stock,selected_first_cards):
             self.hand=random.sample(set(stock)-selected_first_cards,2)
             selected_first_cards.update(self.hand)
             for c in self.hand:
                 stock.remove(c)
+        def take_stockcard(self,stock):
+            #stockを引数で渡さなくていいようにするのがいい気がする
+            stock.pop()
+        def take_rowcard_fin(self):
+            pass
+        def action(self,stock):
+            if input("t(take) or f(fin) : ")=="t":
+                self.take_stockcard(stock)
+            else:
+                self.take_rowcard_fin()
+
     def __init__(self):
         self.stock=list(itertools.chain.from_iterable([i]*9 for i in range(7-2)))
         print(self.stock)
-        self.players=[Coloretto.Player() for i in range(2)]
+        self.players=[Coloretto.Player(),Coloretto.Player(True)]
         print(self.players)
         selected_first_cards=set()
         for p in self.players:
@@ -25,4 +37,10 @@ class Coloretto:
         self.rowcards=[1,2,3]
         print("列カード",self.rowcards)
 
+    def play(self):
+        while self.stock:
+            for p in self.players:
+                p.action(self.stock)
+
 game=Coloretto()
+game.play()
